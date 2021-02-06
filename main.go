ec226141350c3a53d6572fcd617f104d44e56a66
LAShZ <lavch@qq.com>
2021-02-06 21:59:04 +0800

    finish group

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	r := lawf.New()
 	r.GET("/", func(c *lawf.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello LA</h1>")
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
 	})
 
 	r.GET("/hello", func(c *lawf.Context) {
@@ -35,6 +35,31 @@ func main() {
 			"password": c.PostForm("password"),
 		})
 	})
+
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *lawf.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello World!</h1>")
+		})
+
+		v1.GET("/hello", func(c *lawf.Context) {
+			c.String(http.StatusOK, "hello %s, you're at v1: %s\n", c.Query("name"), c.Path)
+		})
+	}
+
+	v2 := r.Group("/v2")
+	{
+		v2.GET("hello/:name", func(c *lawf.Context) {
+			c.String(http.StatusOK, "hello %s, you're at v2: %s\n", c.Param("name"), c.Path)
+		})
+
+		v2.POST("/login", func(c *lawf.Context) {
+			c.JSON(http.StatusOK, lawf.Head{
+				"username": c.PostForm("v2:username"),
+				"password": c.PostForm("v2:password"),
+			})
+		})
+	}
 
 	if err := r.Run(":9999"); err != nil {
 		log.Fatal(err)
